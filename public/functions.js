2e8d153176fa6da0c7776c2225f1e6ade4ad0707
Daniel Flores <daniel.flores@dc.indexa.mx>
2024-03-12 16:57:07 -0600

    Se terminaron todas las vistas de los perfiles

---
+++ public/functions.js
@@ -0,0 +1,18 @@
+let idCodeOut = "Accidente";
+function menuActive(navNum, idCodeIn){
+    var activeMenuElem = document.getElementsByClassName("col-3");
+    /*var activeElem = document.getElementById(idCodeIn);
+    var deactiveElem = document.getElementById(idCodeOut);*/
+    for(var i=0;i<activeMenuElem.length;i++){
+        switch (i == navNum){
+            case true:
+                activeMenuElem[i].classList.add("active");
+                break;
+            default:
+                activeMenuElem[i].classList.remove("active");
+        }
+    }
+    idCodeIn.classList.remove("hide");
+    idCodeOut.classList.add("hide");
+    idCodeOut = idCodeIn;
+}
